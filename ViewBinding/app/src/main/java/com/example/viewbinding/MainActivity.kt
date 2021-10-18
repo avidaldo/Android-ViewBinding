1089763ec34512ad52b9211998e30f069ef503c1
avidaldo <avd.formacion@gmail.com>
2021-10-18 02:29:00 +0200

    cambio de background programaticamente

--- ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.viewbinding
 
 import android.content.Intent
+import android.graphics.Color.GREEN
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Seteo programaticamente el color de fondo a verde
+        findViewById<ConstraintLayout>(R.id.layout).setBackgroundColor(GREEN)
 
         val boton = findViewById<Button>(R.id.button)
         val tv = findViewById<TextView>(R.id.textView)
--- ViewBinding/app/src/main/java/com/example/viewbinding/ViewBindingActivity.kt
+++ ViewBinding/app/src/main/java/com/example/viewbinding/ViewBindingActivity.kt
@@ -1,11 +1,14 @@
 package com.example.viewbinding
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.viewbinding.databinding.ActivityMainBinding
 
+
+/* https://developer.android.com/topic/libraries/view-binding */
 
 /* Activity creada desmarcando "Generate a Layout File" para poder usar el mismo layout en ambas */
 
@@ -13,16 +16,32 @@ import com.example.viewbinding.databinding.ActivityMainBinding
 class ViewBindingActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityMainBinding // View Binding
+    private lateinit var binding: ActivityMainBinding
+    /* View Binding crea automáticamente una binding class a partir del layout en xml. El nombre
+    de esta clase se genera automáticamente (activity_main.xml -> ActivityMainBinding).
+     */
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        /* El método estático inflate instancia la binding class */
+
 
         /* a partir de aquí, tenemos una referencia directa a todas las vistas desde dentro del
-        objeto binding */
+        objeto binding. El id de cada elemento del xml pasa a ser un atributo de binding */
+
+        setContentView(binding.root) // root será referencia al elemento raiz aunque no tuviese id
+
+
+
+        /* Seteo programaticamente el color de fondo a azul
+        con el view binding no es necesario asignar un id al layout */
+        binding.root.setBackgroundColor(Color.BLUE)
+        // aunque binding.layout también funciona, ya que puse ese id para cambiarlo desde MainActivty
+
+
 
         binding.textView.setOnClickListener {
             Toast.makeText(
@@ -35,7 +54,7 @@ class ViewBindingActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
             startActivity(
                 Intent(
-                    this@ViewBindingActivity,
+                    this,
                     MainActivity::class.java
                 )
             )
